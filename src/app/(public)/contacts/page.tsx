73ee06a9cb8fa72c046912de1dfc3c9e13ea8b88
CoderTofu <vioneverforget@gmail.com>
2025-02-22 20:09:48 +0800

    paolo:design tweaks

--- src/app/(public)/contacts/page.tsx
+++ src/app/(public)/contacts/page.tsx
@@ -1,10 +1,8 @@
-import "./contacts.css";
-
 export default function Contacts() {
   return (
     <div className="relative">
       {/* Section with "Have Any Questions?" */}
-      <section className="px-5 sm:p-0 my-16 flex justify-around flex-col sm:flex-row items-center">
+      <section className="px-5 sm:p-0 my-20 flex justify-around flex-col sm:flex-row items-center">
         <div className="max-w-md text-center sm:text-left">
           <h3 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-[#BE9231]">
             Have Any <br /> Questions?
@@ -22,42 +20,54 @@ export default function Contacts() {
         </div>
       </section>
 
-      <div className="flex-col flex lg:flex-row bg-[#D3B778] py-24 px-6 lg:px-[100px] gap-10">
+      <div className="flex-col flex lg:flex-row bg-[#D3B778] py-16 px-6 gap-4 justify-center">
         {/* Email */}
-        <div className="bg-white p-6 rounded-3xl w-[550px] h-[400px] mx-auto">
-          <div className="flex justify-center items-center min-h-[120px]">
-            <img src="/icons/email.png" 
-            alt="email" 
-            className="w-12 h-11" />
+        <div className="bg-white p-3 rounded-3xl h-[400px] flex flex-col justify-center items-center min-w-[300px]">
+          <div className="flex justify-center items-center mb-5">
+            <img src="/icons/email.png" alt="email" className="w-12 h-11" />
           </div>
-          <p className="underline font-bold text-center mt-5 text-lg sm:text-xl md:text-2xl">EMAIL</p>
-          <p className="text-center mt-3 text-gray-500 text-sm sm:text-base md:text-lg">Reach us through our email address</p>
-          <p className="text-center mt-3 text-lg sm:text-xl md:text-2xl break-words">ummphils@gmail.com</p>
+          <p className="underline font-bold text-center mt-5 text-xl sm:text-xl md:text-2xl">
+            EMAIL
+          </p>
+          <p className="text-center mt-3 text-gray-500 text-base sm:text-lg md:text-xl">
+            Reach us through our email address
+          </p>
+          <p className="text-center mt-3 text-base sm:text-lg md:text-xl">
+            ummphils@gmail.com
+          </p>
         </div>
 
         {/* Phone */}
-        <div className="bg-white p-6 rounded-3xl w-[550px] h-[400px] mx-auto">
-          <div className="flex justify-center items-center min-h-[115px]">
-            <img src="/icons/phone.png" 
-            alt="phone" 
-            className="w-11 h-11" />
+        <div className="bg-white p-3 rounded-3xl h-[400px] flex flex-col justify-center items-center min-w-[300px]">
+          <div className="flex justify-center items-center  mb-5">
+            <img src="/icons/phone.png" alt="phone" className="w-11 h-11" />
           </div>
-          <p className="underline font-bold text-center mt-5 text-lg sm:text-xl md:text-2xl">PHONE</p>
-          <p className="text-center mt-3 text-gray-500 text-sm sm:text-base md:text-lg">Reach us through our Phone number</p>
-          <p className="text-center mt-3 text-lg sm:text-xl md:text-2xl break-words">(+63) 924 1833</p>
+          <p className="underline font-bold text-center mt-5 text-lg sm:text-xl md:text-2xl">
+            PHONE
+          </p>
+          <p className="text-center mt-3 text-gray-500 text-base sm:text-lg md:text-xl">
+            Reach us through our Phone number
+          </p>
+          <p className="text-center mt-3 text-base sm:text-lg md:text-xl">
+            (+63) 924 1833
+          </p>
         </div>
 
         {/* Address */}
-        <div className="bg-white p-6 rounded-3xl w-[550px] h-[400px] mx-auto">
-          <div className="flex justify-center items-center min-h-[120px]">
-            <img src="/icons/pin.png" 
-            alt="pin" 
-            className="w-11 h-11" />
+        <div className="bg-white p-3 rounded-3xl h-[400px] flex flex-col justify-center items-center min-w-[300px]">
+          <div className="flex justify-center items-center mb-5">
+            <img src="/icons/pin.png" alt="pin" className="w-11 h-11" />
           </div>
           <div className="text-center justify-center flex flex-col">
-            <p className="underline font-bold mt-5 text-lg sm:text-xl md:text-2xl">ADDRESS</p>
-            <p className="mt-3 text-gray-500 text-sm sm:text-base md:text-lg">Get to know where we're located </p>
-            <p className="mt-3 text-lg sm:text-xl md:text-2xl break-words">Quezon City, Philippines</p>
+            <p className="underline font-bold mt-5 text-lg sm:text-xl md:text-2xl">
+              ADDRESS
+            </p>
+            <p className="text-center mt-3 text-gray-500 text-base sm:text-lg md:text-xl">
+              Get to know where we&apos;re located{" "}
+            </p>
+            <p className="text-center mt-3 text-base sm:text-lg md:text-xl">
+              Quezon City, Philippines
+            </p>
           </div>
         </div>
       </div>
