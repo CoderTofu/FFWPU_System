f2a5d8f6ab524841ce9d5e3f4371f9c267232f84
CoderTofu <vioneverforget@gmail.com>
2025-02-23 15:15:51 +0800

    deleted components

--- src/components/footerPrivate.tsx
+++
@@ -1,7 +0,0 @@
-export default function FooterPrivate() {
-  return (
-    <div className="bg-[#01438F] text-white p-8 text-center">
-      Copyright © 2025, FFWPU. All Rights Reserved.
-    </div>
-  );
-}
