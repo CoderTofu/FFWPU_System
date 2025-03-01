74f3523fc7206c41939c3491a9840d5e1cce526b
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-01 19:46:37 +0800

    implement to sample table

--- src/app/(private)/sample/page.tsx
+++ src/app/(private)/sample/page.tsx
@@ -1,68 +1,89 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "@/components/Table";
 import { useRouter } from "next/navigation";
+import { axiosInstance } from "@/app/axiosInstance";
+import Cookies from "js-cookie";
 
 export default function TryUlit() {
-    const [selectedRow, setSelectedRow] = useState<{ ID: number } | null>(null);
-    const router = useRouter();
+  const [selectedRow, setSelectedRow] = useState<{
+    "Member ID": number;
+  } | null>(null);
+  const router = useRouter();
 
-    const data = [
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-        {ID:12345, Name:"James", Age:21},
-    ]
+  const dummyData = [
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+    { ID: 12345, Name: "James", Age: 21 },
+  ];
 
-    const column = {
-        lg:["ID","Name","Age"],
-        md:["ID","Name"],
-        sm:["Name"]
+  const [data, setData] = useState([]);
+  const column = {
+    lg: ["Member ID", "Full Name", "Age"],
+    md: ["Member ID", "Full Name"],
+    sm: ["Name"],
+  };
+
+  const url = "/user";
+
+  const handleEditClick = () => {
+    if (selectedRow) {
+      router.push(`/members/${selectedRow["Member ID"]}/edit`);
     }
+  };
 
-    const url = "/user"
+  useEffect(() => {
+    axiosInstance
+      .get("/members/", {
+        headers: { Authorization: `Bearer ${Cookies.get("access_token")}` },
+      })
+      .then((res) => {
+        setData(res.data);
+      });
+  }, []);
 
-    const handleEditClick = () => {
-        if (selectedRow) {
-          router.push(`/user/${selectedRow.ID}/edit`);
-        }
-    };
+  return (
+    <div className="p-4 bg-[#D9D9D9] min-h-screen justify-center items-center">
+      <div className="overflow-hidden rounded-lg">
+        <Table
+          data={data}
+          columns={column}
+          rowDoubleClickPath={url}
+          onRowSelect={setSelectedRow}
+        />
+      </div>
 
-    return (<div className="p-4 bg-[#D9D9D9] min-h-screen justify-center items-center">
-
-        <div className="overflow-hidden rounded-lg">
-            <Table data={data} columns={column} rowDoubleClickPath={url} onRowSelect={setSelectedRow}/>
-        </div>
-
-        <div className="flex items-center justify-center h-32">
-            <button
-            onClick={handleEditClick}
-            disabled={!selectedRow}
-            className={`px-4 py-2 rounded mb-4 m-4 justify-center ${
-                selectedRow ? "bg-blue-500 text-white hover:bg-blue-600" 
-                : "bg-gray-300 text-gray-500 cursor-not-allowed"
-            }`}
-            >
-            Edit
-            </button>
-        </div>
-
-    </div>);
-  }
+      <div className="flex items-center justify-center h-32">
+        <button
+          onClick={handleEditClick}
+          disabled={!selectedRow}
+          className={`px-4 py-2 rounded mb-4 m-4 justify-center ${
+            selectedRow
+              ? "bg-blue-500 text-white hover:bg-blue-600"
+              : "bg-gray-300 text-gray-500 cursor-not-allowed"
+          }`}
+        >
+          Edit
+        </button>
+      </div>
+    </div>
+  );
+}
