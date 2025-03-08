ecccc1c8b09922de7e285e391c51a39df3834ba2
Jaems F <jefanio718@gmail.com>
2025-03-08 13:25:28 +0800

    initial commit

---
+++ src/app/(private)/member/display-member/page.tsx
@@ -0,0 +1,27 @@
+export default function DisplayMember() {
+    return (<div>
+    {/*Banner */}
+    <div className=" bg-white p-4 shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+      <p className="mt-4 mb-4 text-center text-2xl sm:text-3xl font-semibold">
+        MEMBER PROFILE
+      </p>
+    </div>
+
+    {/*Section for Member Profile Pic*/}
+    <section></section>
+
+    {/*Section for Personal Information*/}
+    <section></section>
+
+    {/*Section for Spiritual Information*/}
+    <section></section>
+
+    {/*Section for Mission History*/}
+    <section></section>
+
+    {/*Section for List of Blessings*/}
+    <section></section>
+
+
+    </div>);
+}
