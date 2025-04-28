9523d310f2a888bf998b626922e013268b36988b
CoderTofu <vioneverforget@gmail.com>
2025-04-28 22:11:45 +0800

    added clouds and hover animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,15 +89,14 @@ export default function Home() {
                   alt="Heavenly Parent"
                   className="w-full h-[440px] object-cover"
                 />
-                <div className="absolute inset-0 bg-black bg-opacity-30 transition-all duration-500 ease-out group-hover:bg-opacity-50"></div>{" "}
+                <div className="absolute inset-0 bg-black bg-opacity-30 transition-all duration-500 ease-out group-hover:bg-opacity-50"></div>{' '}
                 <div className="absolute inset-0 flex flex-col justify-between p-8 text-white">
                   <h3 className="text-3xl font-semibold font-instrument">
                     God as our Heavenly Parent
                   </h3>
                   <p className="text-base font-instrument">
-                    We believe God is our loving Heavenly Parent, seeking a
-                    close bond with us. Our mission is to rebuild that
-                    connection and unite as one global family.
+                    We believe God is our loving Heavenly Parent, seeking a close bond with us. Our
+                    mission is to rebuild that connection and unite as one global family.
                   </p>
                 </div>
               </motion.div>
@@ -118,9 +117,8 @@ export default function Home() {
                     True Parents and Divine Principle
                   </h3>
                   <p className="text-base font-instrument">
-                    True Parents show God's love and guide our spiritual growth.
-                    The Divine Principle helps us live our purpose and build a
-                    peaceful world.
+                    True Parents show God's love and guide our spiritual growth. The Divine
+                    Principle helps us live our purpose and build a peaceful world.
                   </p>
                 </div>
               </motion.div>
@@ -137,12 +135,10 @@ export default function Home() {
                 />
                 <div className="absolute inset-0 bg-black bg-opacity-30 group-hover:bg-opacity-50 transition-all duration-500 ease-out"></div>
                 <div className="absolute inset-0 flex flex-col justify-between p-8 text-white">
-                  <h3 className="text-3xl font-semibold font-instrument">
-                    The Marriage Blessing
-                  </h3>
+                  <h3 className="text-3xl font-semibold font-instrument">The Marriage Blessing</h3>
                   <p className="text-base font-instrument">
-                    The Marriage Blessing is a sacred vow to God that
-                    strengthens families and builds a world of love and unity.
+                    The Marriage Blessing is a sacred vow to God that strengthens families and
+                    builds a world of love and unity.
                   </p>
                 </div>
               </motion.div>
@@ -154,10 +150,15 @@ export default function Home() {
         <div className="pane w-screen h-screen absolute top-0"></div>
       </section>
 
-      <section
-        className="relative z-10 py-20 px-6 flex flex-col items-center w-full"
-        id="about"
-      >
+      <section className="relative z-10 py-20 px-6 flex flex-col items-center w-full" id="about">
+        {/* clouds */}
+        <div className="absolute inset-0 z-0 overflow-hidden">
+          <div className="cloud3d cloud2"></div>
+          <div className="cloud3d cloud1"></div>
+          <div className="cloud3d cloud3"></div>
+          <div className="cloud3d cloud6"></div>
+          <div className="cloud3d cloud4"></div>
+        </div>
         <motion.div
           initial="hidden"
           whileInView="visible"
@@ -169,13 +170,10 @@ export default function Home() {
               },
             },
           }}
-          className="max-w-[1600px] w-full bg-white px-8 md:px-16 py-12 rounded-lg shadow-2xl flex flex-col lg:flex-row gap-x-6 gap-y-12"
+          className="max-w-[1600px] w-full bg-white px-8 md:px-16 py-12 rounded-lg shadow-2xl flex flex-col lg:flex-row gap-x-6 gap-y-12 items-stretch z-10 relative"
         >
           {/* Left Side */}
-          <motion.div
-            variants={fadeUpVariants}
-            className="flex-1 flex flex-col gap-8"
-          >
+          <motion.div variants={fadeUpVariants} className="flex-1 flex flex-col gap-8 h-full z-10">
             <div>
               <div className="text-6xl md:text-6xl font-extrabold leading-tight font-instrument">
                 <div className="hidden lg:block">
@@ -191,27 +189,30 @@ export default function Home() {
 
             <motion.p
               variants={fadeUpVariants}
-              className="font-instrument text-lg leading-10"
+              className="font-instrument text-lg leading-10 my-10"
             >
-              The Family Federation is a global movement that, like its
-              birthplace South Korea, has experienced remarkable growth. Sun
-              Myung Moon, its founder, was called by Jesus Christ to his mission
-              in 1935, during Korea’s harsh Japanese occupation. After Korea’s
-              liberation in 1945, he faced brutal persecution under North
-              Korea’s communist regime, enduring imprisonment and torture.{" "}
-              <br /> <br />
-              Despite these hardships, he continued preaching and, during the
-              Korean War, founded the Unification Church with a small group of
-              followers. By 1996, the Family Federation had expanded worldwide.
+              The Family Federation is a global movement that, like its birthplace South Korea, has
+              experienced remarkable growth. Sun Myung Moon, its founder, was called by Jesus Christ
+              to his mission in 1935, during Korea’s harsh Japanese occupation. After Korea’s
+              liberation in 1945, he faced brutal persecution under North Korea’s communist regime,
+              enduring imprisonment and torture. <br /> <br />
+              Despite these hardships, he continued preaching and, during the Korean War, founded
+              the Unification Church with a small group of followers. By 1996, the Family Federation
+              had expanded worldwide.
             </motion.p>
 
             <motion.div
+              whileHover={{
+                scale: 1.05,
+                boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.2)',
+                transition: { duration: 0.3 },
+              }}
               variants={fadeUpVariants}
               className="border-[#EFC017] border-4 rounded shadow-lg"
             >
               <img
                 src="/images/about/about1.png"
-                className="w-full h-auto aspect-[303/182]"
+                className="w-full h-auto object-cover max-h-[376px]"
                 alt=""
               />
             </motion.div>
@@ -220,38 +221,44 @@ export default function Home() {
           {/* Right Side */}
           <motion.div
             variants={fadeUpVariants}
-            className="flex-1 flex flex-col gap-8"
+            className="flex-1 flex flex-col gap-8 h-full justify-center z-10"
           >
             <motion.div
+              whileHover={{
+                scale: 1.05,
+                boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.2)',
+                transition: { duration: 0.3 },
+              }}
               variants={fadeUpVariants}
               className="border-[#EFC017] border-4 rounded shadow-lg"
             >
               <img
                 src="/images/about/about2.png"
-                className="w-full h-auto aspect-[562.87/376.00]"
+                className="w-full h-auto object-cover max-h-[376px]"
                 alt=""
               />
             </motion.div>
 
-            <motion.p
-              variants={fadeUpVariants}
-              className="font-instrument text-lg leading-10"
-            >
-              The Divine Principle, Father Moon’s core teachings, was originally
-              written for Christians but later broadened to a wider audience.
-              The Family Federation became the first movement of its kind
-              founded by a couple—Father and Mother Moon. Since Father Moon’s
-              passing in 2012, Mother Moon has continued spreading their vision,
-              gaining recognition from major religious groups
+            <motion.p variants={fadeUpVariants} className="font-instrument text-lg leading-10">
+              The Divine Principle, Father Moon’s core teachings, was originally written for
+              Christians but later broadened to a wider audience. The Family Federation became the
+              first movement of its kind founded by a couple—Father and Mother Moon. Since Father
+              Moon’s passing in 2012, Mother Moon has continued spreading their vision, gaining
+              recognition from major religious groups
             </motion.p>
 
             <motion.div
+              whileHover={{
+                scale: 1.05,
+                boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.2)',
+                transition: { duration: 0.3 },
+              }}
               variants={fadeUpVariants}
               className="border-[#EFC017] border-4 rounded shadow-lg"
             >
               <img
                 src="/images/about/about3.png"
-                className="w-full h-auto aspect-[3/2]"
+                className="w-full h-auto object-cover max-h-[376px]"
                 alt=""
               />
             </motion.div>
